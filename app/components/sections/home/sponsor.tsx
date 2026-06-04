54830b54560c359b92015098e8e8f6d41e93e21f
Dima M Dinama <dmaharika@gmail.com>
2026-06-04 01:53:54 +0000

    edit sponsorship page and section

--- app/components/sections/home/sponsor.tsx
+++ app/components/sections/home/sponsor.tsx
@@ -1,13 +1,13 @@
 export const SponsorSection = () => {
 	return (
-		<section className="pt-18 sm:pt-36 bg-[#F1F1F1] relative">
+		<section className="pt-18 sm:pt-36 bg-[#F1F2F3] relative">
 			<div className="container mx-auto text-white px-5 2xl:px-0">
 				<div className="flex flex-col items-center">
 					<div className="h-max mb-12 md:mb-12 md:top-12 text-center">
-						<p className="text-primary font-display font-medium text-2xl md:text-3xl mb-4">
+						<p className="text-black font-display font-medium text-2xl md:text-3xl mb-4">
 							We appreciate your support!
 						</p>
-						<h1 className="text-blue-900 font-display font-bold text-4xl md:text-[2.5rem] lg:text-[3.5rem] leading-tight mb-10">
+						<h1 className="text-black font-display font-bold text-4xl md:text-[2.5rem] lg:text-[3.5rem] leading-tight mb-10">
 							Help us make this conference truly unforgettable
 						</h1>
 						<a
@@ -17,21 +17,21 @@ export const SponsorSection = () => {
 						>
 							<button
 								type="button"
-								className="cursor-pointer text-2xl px-8 py-4 rounded-3xl font-bold bg-gradient-to-r from-[#224083] via-[#224083] to-[#2D66E8] hover:from-[#2D66E8] hover:to-[#224083] transition-all duration-300"
+								className="cursor-pointer text-2xl px-8 py-4 rounded-3xl font-bold bg-black hover:bg-black/50 transition-all duration-300"
 							>
-								Sponsorship Proposal
+								Be Our Sponsor
 							</button>
 						</a>
 					</div>
 
 					<div className="p-5 text-center text-bold text-blue-900 text-2xl max-w-2xl">
 						<div>
-							<h3 className="mb-5 font-display font-bold text-4xl">
+							{/* <h3 className="mb-5 font-display font-bold text-4xl">
 								Our Sponsors
-							</h3>
+							</h3> */}
 							<div className="grid grid-cols-1 gap-5">
 								{/* Ultimate Sponsor Section */}
-								<div className="grid grid-cols-1 w-full items-center justify-center gap-5">
+								{/* <div className="grid grid-cols-1 w-full items-center justify-center gap-5">
 									<p className="text-xl font-medium">Ultimate Sponsor</p>
 									<a
 										href="https://serpapi.com/"
@@ -44,9 +44,9 @@ export const SponsorSection = () => {
 											className="object-cover max-h-70 mx-auto"
 										/>
 									</a>
-								</div>
+								</div> */}
 								{/* Silver Sponsor Section */}
-								<div className="grid grid-cols-1 w-full items-center justify-center gap-5">
+								{/* <div className="grid grid-cols-1 w-full items-center justify-center gap-5">
 									<p className="text-xl font-medium">Silver Sponsor</p>
 									<a
 										href="https://www.navicat.com/en/"
@@ -59,7 +59,7 @@ export const SponsorSection = () => {
 											className="object-cover max-h-50 mx-auto"
 										/>
 									</a>
-								</div>
+								</div> */}
 							</div>
 						</div>
 					</div>
